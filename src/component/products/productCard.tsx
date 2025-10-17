98bcc0e5cd45af621d05fa78e4ae78e2c851b418
nazmuShakib <nazmushakib6@gmail.com>
2025-10-18 01:43:09 +0600

    fix product card hover issue

--- src/component/products/productCard.tsx
+++ src/component/products/productCard.tsx
@@ -16,8 +16,8 @@ interface ProductCardProps {
 
 const ProductCard: FC<ProductCardProps> = ({ product }) => {
   return (
-    <Link href={`/products/${product.slug}`}>
-      <div className="bg-foreground/15 rounded-lg shadow-lg overflow-hidden transition-transform duration-400 ease-in-out hover:cursor-pointer hover:scale-[0.992] hover:shadow-2xl">
+    <Link href={`/products/${product.slug}`} className="block group">
+      <div className="bg-foreground/15 rounded-lg shadow-lg overflow-hidden transition-all duration-200 ease-in-out hover:cursor-pointer hover:scale-[0.992] hover:shadow-2xl">
         <div className="w-full h-48 bg-gray-200 flex items-center justify-center">
           {product.images && product.images.length > 0 ? (
             <img
@@ -29,7 +29,7 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
             <span className="text-gray-500 text-lg">No Image</span>
           )}
         </div>
-        <div className="p-4">
+        <div className="p-4 transition-all duration-400 ease-in-out group-hover:scale-[1.008]">
           <h2 className="text-xl font-semibold truncate">{product.name}</h2>
           <p className="text-lg mt-2">${product.price}</p>
           <button className="mt-4 w-full bg-success text-white py-2 rounded hover:bg-success/85 transition-colors duration-300">
